e54aeba07e057417b4e4cf0875bc614bc4dcd11f
Sharon Watkins <swatkins876@gmail.com>
2019-08-06 20:19:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# Your Code Here
+def my_own_map(1, 2, 3, -9)
+   new = []
+   while i < my_own_map.length do
+     new.push(my_own_map[i] * -1)
+     i += -1
+   end
+   yield 
+ end 
+  
+  def dune(["paul", "gurney", "vladimir", "jessica", "chani"])
+    new = []
+    while i <= dune.length do 
+      new.push(dune[i] == dune)
+      i += 1 
+    end
+    yield
+  end 
+  
+  def map([1, 2, 3, -9])
+    new = []
+    while i < map.length do 
+      new.push(map[i] * 2)
+      i += 1 
+    end
+    yield
+  end 
+  
+  def map([1, 2, 3, -9])
+    new = []
+    while i < map.length do 
+      new.push(map[i] ** 2)
+      i += 1 
+    end
+    yield
+  end 
+  
+  
